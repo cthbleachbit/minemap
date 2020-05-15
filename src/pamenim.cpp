f8af2e9008b5225eb88b7b799805dc40b579a7a3
Tianhao Chai <cth451@gmail.com>
2020-05-15 15:46:29 -0500

    restart pamenim

---
+++ src/pamenim.cpp
@@ -0,0 +1,21 @@
+/* Minemap
+ *
+ * pamenim: Main program: convert map to png
+ */
+
+#include <cstdio>
+
+void usage() {
+	printf("pamenim <options>\n");
+	printf("\t-i, --input INPUT\n");
+	printf("\t\tRequired, gzipped nbt input\n");
+	printf("\t-g, --game VER\n");
+	printf("\t\tRequired, Minecraft game version this map is exported from\n");
+	printf("\t-o, --output OUTPUT\n");
+	printf("\t\tRequired, output picture\n");
+}
+
+int main(int argc, char **argv) {
+	// STUB
+	return 0;
+}
